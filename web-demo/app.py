728d10ed88435e059422118e3976bf86d0a22df7
ikaros55439147 <a119551773@gmail.com>
2025-07-09 06:04:45 +0000

    1.3

--- web-demo/app.py
+++ web-demo/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template 
 import psycopg2 # <--- 更改這裡：使用 psycopg2 連接 PostgreSQL
 
 app = Flask(__name__)
